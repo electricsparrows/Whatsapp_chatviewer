ea9e250d83bb45877a515f9961c1c87e50748fb7
electricsparrows <20927172+electricsparrows@users.noreply.github.com>
2021-08-29 22:35:06 +0100

    implemented the conversation splitter

---
+++ conversationSplitter.py
@@ -0,0 +1,73 @@
+import datetime
+import string
+from datetime import datetime as dt
+from pathlib import Path
+import db
+import filehandler as fh
+
+# globals
+test_files_dir = "C:\\Users\\Cindy\\PycharmProjects\\ChatViewer-testfiles"
+p = Path(test_files_dir)
+
+in_file = p / "test02.txt"
+out_file = Path.cwd() / "out" / "output.xlsx"
+
+
+def conversation_splitter(data):
+    """
+    :param data: data should be messages with same date, import_ref (indicating imported from the same file,
+    therefore the same chat thread), and should have a consistent group of participants. It is assumed that
+    message records are sorted in chronological order and in order by msg_id (indexed).
+
+    - it is assumed that all messages belong to one or more conversations.
+    :return: list of pointers to convo_heads
+    """
+    timegap_threshold = datetime.timedelta(seconds=3600)    # 1 hr gap between messages
+    convo_heads= []
+    prev_dtstr = None
+
+    for m in data:
+        # if msg_content contains any greeting, reduce time-gap threshold to 30 mins
+        if has_greetings(m['msg_content']):
+            timegap_threshold = datetime.timedelta(seconds=1800)
+
+        if calc_timedelta(prev_dtstr, m['date_time']) > timegap_threshold:
+            convo_heads.append(m['msg_id'])
+        prev_dtstr = m['date_time']
+    return convo_heads
+
+
+def convert_dtstr_to_datetime(dt_str: str) -> datetime.datetime:
+    return dt.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
+
+
+def has_greetings(msg_content: str) -> bool:
+    greetings = ("hi", "hello", "hey", "yo", "hiya")
+    words = msg_content.lower().split()         # this gives unigrams
+    for i in range(len(words)):
+        words[i] = words[i].strip(string.punctuation)
+    for g in greetings:
+        if g in words:
+            return True
+    return False
+
+
+def calc_timedelta(time1: str, time2: str) -> datetime.timedelta:
+    """converts time strings to datetime objs; it is assumed that time2 is after time1 (t2 > t1)"""
+    if time1 is None or time2 is None:
+        return datetime.timedelta(seconds=0)
+    dt1 = convert_dtstr_to_datetime(time1)
+    dt2 = convert_dtstr_to_datetime(time2)
+    return abs(dt2 - dt1)  # timedelta in seconds
+
+
+def main():
+    data = db.get_msgs_at_date("2016-01-06")
+    ptrs = conversation_splitter(data)
+
+    for ch in ptrs:
+        print(db.read_msg(ch)['msg_content'])
+
+
+if __name__ == '__main__':
+    main()
